88adc224350161393e29d9784179572a601a39b0
djm2x <dj-m2x@hotmail.com>
2021-01-03 21:45:07 +0100

    test json to list

--- Program.cs
+++ Program.cs
@@ -5,23 +5,44 @@ using Newtonsoft.Json;
 
 namespace console_net
 {
-    class Axe {
-        public int Id { get; set; }
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            var list = new AxeController().List();
+
+            Console.WriteLine(JsonConvert.SerializeObject(list));
+
+        }
+    }
+
+    class AxeController {
+        protected List<Axe> Axes = new List<Axe>{ new Axe {Id = 1}, new Axe {Id = 2} };
+
+        public List<Axe> List() {
             string json = @"[3,4,7,1]";
 
-            var list = JsonConvert.DeserializeObject<List<int>>(json);
+            string json2 = @"3";
 
-            var Axes = new List<Axe>{ new Axe {Id = 1}, new Axe {Id = 2} };
+            var list = Axes.Where(e => JsonArrayIntToListInt(json2).Contains(e.Id)).ToList();
 
-            var r = Axes.Where(e => list.Contains(e.Id)).ToList();
+            return list;
 
-
-            Console.WriteLine(JsonConvert.SerializeObject(r));
         }
+
+        private List<int> JsonArrayIntToListInt(string jsonArrayOfInt) {
+
+            if (!jsonArrayOfInt.Contains("["))
+            {
+                jsonArrayOfInt = $"[{jsonArrayOfInt}]";
+            }
+
+            return JsonConvert.DeserializeObject<List<int>>(jsonArrayOfInt);
+        }
+    }
+
+    class Axe {
+        public int Id { get; set; }
     }
 }
