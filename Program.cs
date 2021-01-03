757722b59ccb962ee2be875e9873db9805c8afe2
djm2x <dj-m2x@hotmail.com>
2021-01-03 15:04:32 +0100

    test json to list

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace console_net
+{
+    class Axe {
+        public int Id { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string json = @"[3,4,7,1]";
+
+            var jarray = JsonConvert.DeserializeObject<List<int>>(json);
+
+            var Axes = new List<Axe>{ new Axe {Id = 1}, new Axe {Id = 2} };
+
+            var r = Axes.Where(e => jarray.Contains(e.Id)).ToList();
+
+
+            Console.WriteLine(JsonConvert.SerializeObject(r));
+        }
+    }
+}
---
+++ readMe.md
@@ -0,0 +1,13 @@
+# App web DIDH
+
+## Le projet à éte developpé par les technologies suivantes : 
+[ASP.net Core 5.0 SDK](https://dotnet.microsoft.com/download)
+
+[Git for Windows](https://git-scm.com/downloads)
+
+## Etape d'instalation : 
+
+### clone le repo
+```
+git clone https://github.com/djm2x/console_net
+```
